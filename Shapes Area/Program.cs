bfb4bc5fad861933dfeb83fb948767c1fa2f42b8
Mohamed Hegazy <mhegazy22111998@gmail.com>
2020-12-26 19:39:01 +0200

    added GetShape function

--- Shapes Area/Program.cs
+++ Shapes Area/Program.cs
@@ -14,7 +14,13 @@ namespace Shapes_Area
             while (true)
             {
                 String input = Console.ReadLine();
-                getArea(input);
+                //getArea(input);
+                Shape shape = GetShape(input);
+                if (shape != null)
+                    Console.WriteLine("Shape Area = " + shape.Area());
+                else
+                    continue;
+           
                 Console.WriteLine("enter 1 to end the program or any key to do continue : ");
                 string y = Console.ReadLine();
                 if (y.Equals("1"))
@@ -27,10 +33,10 @@ namespace Shapes_Area
         public static void getArea(String input)
         {
             string[] values = input.Split();
-            if (values.Length == 2 && values[0]=="c")
+            if (values.Length == 2 && values[0] == "c")
             {
                 var circle = new Circle() { Radius = float.Parse(values[1]) };
-                Console.WriteLine("Circle Area = "+circle.Area());
+                Console.WriteLine("Circle Area = " + circle.Area());
             }
             else if (values.Length == 3 && values[0] == "s")
             {
@@ -41,6 +47,34 @@ namespace Shapes_Area
             {
                 var rectangle = new Rectangle() { Hight = float.Parse(values[1]), width = float.Parse(values[2]) };
                 Console.WriteLine("Rectangle Area = " + rectangle.Area());
+            }
+            else
+            {
+                Console.WriteLine("invalid input");
+            }
+        }
+        public static Shape GetShape(string input)
+        {
+            string[] values = input.Split();
+            if (values.Length == 2 && values[0] == "c")
+            {
+                var circle = new Circle() { Radius = float.Parse(values[1]) };
+                return circle;
+            }
+            else if (values.Length == 3 && values[0] == "s")
+            {
+                var square = new Square() { Hight = float.Parse(values[1]), width = float.Parse(values[2]) };
+                return square;
+            }
+            else if (values.Length == 3 && values[0] == "r")
+            {
+                var rectangle = new Rectangle() { Hight = float.Parse(values[1]), width = float.Parse(values[2]) };
+                return rectangle;
+            }
+            else
+            {
+                Console.WriteLine("invalid input");
+                return null;
             }
         }
        
